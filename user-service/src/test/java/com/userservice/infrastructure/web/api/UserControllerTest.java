44e603fb940cf8f6d15b2d4d68889d35df811013
choizz156 <tkfka156@gmail.com>
2025-11-05 20:07:11 +0900

    test: 회원가입 api 테스트 구성 #3

---
+++ user-service/src/test/java/com/userservice/infrastructure/web/api/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.userservice.infrastructure.web.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.userservice.infrastructure.web.dto.UserCreateRequest;
+
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DisplayName("신규 유저를 생성한다.")
+	@Test
+	void test1() throws Exception {
+		// given
+		UserCreateRequest request = new UserCreateRequest("test@test.com", "password", "010-1111-0111", "street",
+			"123423", "state", "city", "details", "name", "nickname", "profileImageUrl");
+
+		// when & then
+		mockMvc.perform(post("/api/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("가입 완료"))
+			.andExpect(jsonPath("$.data.nickname").value("nickname"))
+			.andExpect(jsonPath("$.data.profileUrl").value("profileImageUrl"))
+			.andExpect(jsonPath("$.data.userId").value(Matchers.notNullValue()));
+	}
+}
