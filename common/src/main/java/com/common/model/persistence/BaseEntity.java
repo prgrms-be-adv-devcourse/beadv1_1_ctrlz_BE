820c490ffd27c3bd35cfefdb7c595409cf1afbc8
Leegeonmin <kmlee97s@gmail.com>
2025-11-05 09:47:54 +0900

    feat : BaseEntity @Getter 추가, id 타입 UUID로 변경

--- common/src/main/java/com/common/model/persistence/BaseEntity.java
+++ common/src/main/java/com/common/model/persistence/BaseEntity.java
@@ -1,84 +1,88 @@
 package com.common.model.persistence;
 
-import com.github.f4b6a3.uuid.UuidCreator;
-import jakarta.persistence.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	private String id;
 
-    @Column(nullable = false, unique = true, updatable = false)
-    private String code;
+	@Enumerated(EnumType.STRING)
+	private DeleteStatus deleteStatus = DeleteStatus.N;
 
-    @Enumerated(EnumType.STRING)
-    private DeleteStatus deleteStatus = DeleteStatus.N;
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
 
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
+	protected abstract String getEntitySuffix(); // 각 엔티티가 구현해야 하는 추상 메서드
 
-    protected abstract String getEntityPrefix(); // 각 엔티티가 구현해야 하는 추상 메서드
+	@PrePersist // persist() 를 호출하기 전, 새로운 엔티티가 영속성 컨텍스트에 관리되기 직전에 호출
+	protected void onCreate() {
 
-    @PrePersist // persist() 를 호출하기 전, 새로운 엔티티가 영속성 컨텍스트에 관리되기 직전에 호출
-    protected void onCreate() {
+		String UUIDv7 = UuidCreator.getTimeOrderedEpoch().toString();
+		this.id = UUIDv7 + getEntitySuffix();
 
-        String UUIDv7 = UuidCreator.getTimeOrderedEpoch().toString();
-        this.code = getEntityPrefix() + "-" + UUIDv7;
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
 
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
+	public void delete() {
+		this.deleteStatus = DeleteStatus.D;
+		this.updatedAt = LocalDateTime.now();
+	}
 
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-    public void delete() {
-        this.deleteStatus = DeleteStatus.D;
-        this.updatedAt = LocalDateTime.now();
-    }
+		BaseEntity that = (BaseEntity)o;
+		return Objects.equals(id, that.id) && Objects.equals(createdAt, that.createdAt);
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        BaseEntity that = (BaseEntity) o;
-        return Objects.equals(id, that.id) && Objects.equals(code, that.code);
-    }
+	@Override
+	public int hashCode() {
+		int result = Objects.hashCode(id);
+		result = 31 * result + Objects.hashCode(createdAt);
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, code);
-    }
+	public enum DeleteStatus {
+		N, D
+	}
 
-    public enum DeleteStatus {
-        N, D
-    }
+	public String getId() {
+		return id;
+	}
 
-    /* Getter */
-    public Long getId() {
-        return id;
-    }
+	public DeleteStatus getDeleteStatus() {
+		return deleteStatus;
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
 
-    public DeleteStatus getDeleteStatus() {
-        return deleteStatus;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
 }
