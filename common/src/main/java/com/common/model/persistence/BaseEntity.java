8afa63a1a82f72cd1d600285dea5a0b5dbdb6e69
OJH <ok6737@naver.com>
2025-11-05 09:25:19 +0900

    Feature/#7 domain-service 구조를 모듈러 방식에서 모놀리식 아키텍처로 전환 (#8)
    * feat: Common 모듈에 baseEntity 및 기본 응답 클래스 생성
    
    * docs: 여러 기능별 모듈로 분리되어있던 domain-service를 하나의 프로젝트로 통합
    
    - 강사님 피드백 후 모듈러 방식의 현재 프로젝트 구조는 추후 문제가 발생할 수 있다고 판단
    - 각각의 모듈로 분리되어있던 구조를 하나의 패키지로 통합
    - 일반적인 모놀리식 아키텍처로 개발 진행 예정
    
    * feat: common에 존재하는 baseEntity 및 baseResponse 사용 방법 간단히 작성
    
    - 각 엔티티의 id를 그냥 UUID가 아닌 user-UUID, product-UUID 형태로 구현할 수 있도록 추상메소드로 상속받음
    - getEntityPrefix() 메서드를 구현하여 BaseEntity의 code 값이 자동 설정됨
    
    * feat: 생성, 수정일자 명시적으로 초기화
    
    * feat: 기존 UUIDv4 생성 방식을 UUIDv7 생성 방식으로 변경
    # Conflicts:
    #	common/src/main/java/com/common/model/persistence/BaseEntity.java
    #	common/src/main/java/com/common/model/web/BaseResponse.java
    #	common/src/main/resources/application.yml

--- common/src/main/java/com/common/model/persistence/BaseEntity.java
+++ common/src/main/java/com/common/model/persistence/BaseEntity.java
@@ -1,78 +1,84 @@
 package com.common.model.persistence;
 
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import com.github.f4b6a3.uuid.UuidCreator;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import lombok.Getter;
-
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-	@Id
-	private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Enumerated(EnumType.STRING)
-	private DeleteStatus deleteStatus = DeleteStatus.N;
+    @Column(nullable = false, unique = true, updatable = false)
+    private String code;
 
-	@Column(nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+    @Enumerated(EnumType.STRING)
+    private DeleteStatus deleteStatus = DeleteStatus.N;
 
-	@Column(nullable = false)
-	private LocalDateTime updatedAt;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-	@Deprecated
-	protected abstract String getEntitySuffix(); // 각 엔티티가 구현해야 하는 추상 메서드
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
-	@PrePersist // persist() 를 호출하기 전, 새로운 엔티티가 영속성 컨텍스트에 관리되기 직전에 호출
-	protected void onCreate() {
-		this.id = createEntityId();
-		this.createdAt = LocalDateTime.now();
-		this.updatedAt = LocalDateTime.now();
-	}
+    protected abstract String getEntityPrefix(); // 각 엔티티가 구현해야 하는 추상 메서드
 
-	public static String createEntityId() {
-		return UuidCreator.getTimeOrderedEpoch().toString();
-	}
+    @PrePersist // persist() 를 호출하기 전, 새로운 엔티티가 영속성 컨텍스트에 관리되기 직전에 호출
+    protected void onCreate() {
 
-	public void delete() {
-		this.deleteStatus = DeleteStatus.D;
-		this.updatedAt = LocalDateTime.now();
-	}
+        String UUIDv7 = UuidCreator.getTimeOrderedEpoch().toString();
+        this.code = getEntityPrefix() + "-" + UUIDv7;
 
-	public void update() {
-		this.updatedAt = LocalDateTime.now();
-	}
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
 
-	@Override
-	public boolean equals(Object o) {
-		if (o == null || getClass() != o.getClass())
-			return false;
+    }
 
-		BaseEntity that = (BaseEntity)o;
-		return Objects.equals(id, that.id) && Objects.equals(createdAt, that.createdAt);
-	}
+    public void delete() {
+        this.deleteStatus = DeleteStatus.D;
+        this.updatedAt = LocalDateTime.now();
+    }
 
-	@Override
-	public int hashCode() {
-		int result = Objects.hashCode(id);
-		result = 31 * result + Objects.hashCode(createdAt);
-		return result;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(code, that.code);
+    }
 
-	public enum DeleteStatus {
-		N, D
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code);
+    }
+
+    public enum DeleteStatus {
+        N, D
+    }
+
+    /* Getter */
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public DeleteStatus getDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
