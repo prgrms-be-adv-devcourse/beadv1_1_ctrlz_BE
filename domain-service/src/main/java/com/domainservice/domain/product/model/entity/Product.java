4a40ef70bdf957ac21244a61360e45849908033c
OJH <ok6737@naver.com>
2025-11-05 09:25:19 +0900

    Feature/#7 domain-service 구조를 모듈러 방식에서 모놀리식 아키텍처로 전환 (#8)
    * feat: Common 모듈에 baseEntity 및 기본 응답 클래스 생성
    
    * docs: 여러 기능별 모듈로 분리되어있던 domain-service를 하나의 프로젝트로 통합
    
    - 강사님 피드백 후 모듈러 방식의 현재 프로젝트 구조는 추후 문제가 발생할 수 있다고 판단
    - 각각의 모듈로 분리되어있던 구조를 하나의 패키지로 통합
    - 일반적인 모놀리식 아키텍처로 개발 진행 예정
    
    * feat: common에 존재하는 baseEntity 및 baseResponse 사용 방법 간단히 작성
    
    - 각 엔티티의 id를 그냥 UUID가 아닌 user-UUID, product-UUID 형태로 구현할 수 있도록 추상메소드로 상속받음
    - getEntityPrefix() 메서드를 구현하여 BaseEntity의 code 값이 자동 설정됨
    
    * feat: 생성, 수정일자 명시적으로 초기화
    
    * feat: 기존 UUIDv4 생성 방식을 UUIDv7 생성 방식으로 변경

---
+++ domain-service/src/main/java/com/domainservice/domain/product/model/entity/Product.java
@@ -0,0 +1,36 @@
+package com.domainservice.domain.product.model.entity;
+
+import com.common.model.persistence.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Product extends BaseEntity {
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    /*
+    ...
+     */
+
+    @Override
+    protected String getEntityPrefix() {
+        return "product"; // 해당 메서드를 통해 "product-UUID" 형태의 code가 자동 생성됨
+    }
+
+    @Builder
+    public Product(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+}
