792171a081b0aaa824774d9a5a192fb928c86748
Leegeonmin <kmlee97s@gmail.com>
2025-11-06 14:39:53 +0900

    feat : Cart 엔티티 user관계 id로 변경

--- domain-service/src/main/java/com/domainservice/domain/cart/model/entity/Cart.java
+++ domain-service/src/main/java/com/domainservice/domain/cart/model/entity/Cart.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.common.model.persistence.BaseEntity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -23,11 +24,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Cart extends BaseEntity {
 
-	// @OneToOne(fetch = FetchType.LAZY)
-	// @JoinColumn(name = "user_id", nullable = false)
-	// private UserEntity user;
+	@Column(name = "user_id", nullable = false)
+	private String userId;
 
-	@Builder.Default
 	@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<CartItem> cartItems = new ArrayList<>();
 
@@ -40,12 +39,6 @@ public class Cart extends BaseEntity {
 
 	// 장바구니 아이템 추가
 	public void addCartItem(CartItem cartItem) {
-		// for (CartItem item : cartItems) {
-		// TODO: product 비교 로직 (product 추가 후 변경)
-		// if (item.getProduct().equals(cartItem.getProduct())) {
-		//     item.updateQuantity(item.getQuantity() + cartItem.getQuantity());
-		//     return;
-		// }
 		cartItems.add(cartItem);
 		cartItem.setCart(this);
 	}
