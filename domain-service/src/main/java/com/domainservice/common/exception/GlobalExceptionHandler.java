5ef4a4f60d19f39a53033615177b64cd3a2935aa
jeonghyeon-ohk <ok6737@naver.com>
2025-11-06 12:17:14 +0900

    feat: 입력값 검증(validate), 잘못된 api, 기타 예외에 대한 handleException 구현

--- domain-service/src/main/java/com/domainservice/common/exception/GlobalExceptionHandler.java
+++ domain-service/src/main/java/com/domainservice/common/exception/GlobalExceptionHandler.java
@@ -7,21 +7,75 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    /**
+     * CustomException를 상속받는 모든 예외 처리
+     */
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 
-        ErrorResponse response = ErrorResponse.from(e);
         HttpStatus status = HttpStatus.valueOf(e.getCode());
+        ErrorResponse response = ErrorResponse.of(e.getCode(), e.getMessage());
 
         return ResponseEntity.status(status).body(response);
+    }
+
+    /**
+     * Validation 예외 처리 (400)
+     * 입력값이 잘못 들어온 경우
+     * 해당 입력값의 field, rejectedValue, reason을 확인할 수 있음
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException e) {
+
+        // ErrorResponse 생성
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST.value(),
+                "입력값 검증에 실패했습니다.",
+                e.getBindingResult()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(response);
+
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoApiException(NoResourceFoundException e) {
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.BAD_REQUEST.value()
+                , "요청하신 API 엔드포인트를 찾을 수 없습니다."
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+
+    /**
+     * 기타 걸러지지 않은 오류 발생 시 (500)
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+
+        ErrorResponse response = ErrorResponse.of(
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+                , "서버 내부 오류가 발생했습니다."
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+
     }
 
 }
