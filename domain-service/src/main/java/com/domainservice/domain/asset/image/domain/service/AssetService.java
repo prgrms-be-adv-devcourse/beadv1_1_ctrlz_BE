1e8e182ed89ce9c184eec810e417d94b7c4c1a48
OJH <ok6737@naver.com>
2025-11-11 08:49:40 +0900

    Feature/#35 s3 service 관련 로직 수정 (#38)
    * feat: S3 이미지 업로드 기능 개선 및 ImageTarget 타입 추가 #35
    
    - ImageTarget enum에 NONE 타입 추가하여 타겟 미지정 케이스 처리
    - 단일/다중 이미지 업로드 메서드를 Target 기반으로 통합
      - uploadProfileImage: Target을 지정하지 않으면 NONE으로 업로드
      - uploadProfileImageByTarget: USER/PRODUCT/REVIEW 등 Target 명시 업로드
      - uploadProfileImageList: 여러 파일을 NONE 타입으로 일괄 업로드
      - uploadProfileImageListByTarget: 여러 파일을 특정 Target으로 일괄 업로드
    
    * feat: 이미지 수정 및 삭제 메서드 통일

--- domain-service/src/main/java/com/domainservice/domain/asset/image/domain/service/AssetService.java
+++ domain-service/src/main/java/com/domainservice/domain/asset/image/domain/service/AssetService.java
@@ -1,13 +1,18 @@
 package com.domainservice.domain.asset.image.domain.service;
 
+import com.domainservice.domain.asset.image.domain.entity.Image;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.domainservice.domain.asset.image.domain.entity.Image;
+import java.util.List;
 
 public interface AssetService<T> {
+    Image uploadProfileImage(MultipartFile file);
+    List<Image> uploadProfileImageList(List<MultipartFile> files);
 
-	T uploadUserProfile(MultipartFile file);
-	T getImage(String id);
-	Image updateProfileImage(MultipartFile profileImage, String imageId);
-	void delete(String id);
+    Image updateProfileImage(MultipartFile profileImage, String imageId);
+
+    void deleteProfileImageById(String imageId);
+    void deleteProfileImageByS3Url(String s3Url);
+
+    T getImage(String id);
 }
