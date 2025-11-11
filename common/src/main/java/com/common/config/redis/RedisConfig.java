ba4fb387a027be617da959ef46f4c2fd21b5a1e7
jeonghyeon-ohk <ok6737@naver.com>
2025-11-11 22:29:21 +0900

    fix: RedisTemplate<String, String> 빈으로 등록 시 StringRedisTemplate 과 충돌하여 Spring이 빈을 선택할 수 없는 문제 해결
    - 기본적으로 생성되는 StringRedisTemplate은 RedisTemplate<String, String> 타입
    - 타입이 겹치기 때문에 스프링이 어떤 빈을 주입할지 모르는 현상
    - RedisTemplate<String, Object> 같이 제네릭 타입을 다르게 하여 빈 충돌을 피할 수 있으
    - 우선순위 지정(@Primary)으로 내 빈을 우선 선택해 문제 해결

--- common/src/main/java/com/common/config/redis/RedisConfig.java
+++ common/src/main/java/com/common/config/redis/RedisConfig.java
@@ -3,6 +3,7 @@ package com.common.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -31,6 +32,7 @@ public class RedisConfig {
      * 최근 본 상품 등 단순 문자열 저장용 RedisTemplate
      */
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
