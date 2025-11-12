64833ccba10f7ee71fbd41322321ed514b70f9c1
choizz156 <tkfka156@gmail.com>
2025-11-12 17:52:33 +0900

    refactor: 불필요 클래스 제거 #42

--- account-service/user/src/main/java/com/user/infrastructure/redis/vo/CacheType.java
+++ account-service/user/src/main/java/com/user/infrastructure/redis/vo/CacheType.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:account-service/user/src/main/java/com/user/infrastructure/cache/vo/CacheType.java
-package com.user.infrastructure.cache.vo;
-========
 package com.user.infrastructure.redis.vo;
->>>>>>>> choizz156-refactor/#46-판매자-등록-및-로그인-데이터-수정:account-service/user/src/main/java/com/user/infrastructure/redis/vo/CacheType.java
 
 public enum CacheType {
 	VERIFICATION_TRY, VERIFICATION_CODE, VERIFICATION_BAN_ONE_DAY
