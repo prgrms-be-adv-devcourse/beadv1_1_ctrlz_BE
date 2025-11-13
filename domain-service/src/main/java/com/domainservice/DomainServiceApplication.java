005f649af9cb916dde3c1ee24af675f01eb45fd2
junho <>
2025-11-14 00:48:26 +0900

    feat: feignClient 설정 및 회원 정보 가져오는 로직 추가(#51) - 로깅, 예외처리 구현 - FeignClient로 회원 정보 받아오는 로직 추가

--- domain-service/src/main/java/com/domainservice/DomainServiceApplication.java
+++ domain-service/src/main/java/com/domainservice/DomainServiceApplication.java
@@ -2,8 +2,12 @@ package com.domainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.domainservice", "com.common"})
+@SpringBootApplication(scanBasePackages = {"com.domainservice", "com.common.*"})
+@EnableFeignClients(
+	basePackages = {"com.domainservice.common.feign.client"}
+)
 public class DomainServiceApplication {
 
 	public static void main(String[] args) {
