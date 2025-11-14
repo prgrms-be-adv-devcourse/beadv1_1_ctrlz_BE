eaee0e4e1c673f9ff9e71467538c713a2e147d3d
choizz156 <tkfka156@gmail.com>
2025-11-14 16:51:02 +0900

    refactor: 카프카 설정 토픽 분리 #63

--- account-service/user/src/main/java/com/user/infrastructure/kafka/configuration/KafkaConfiguration.java
+++ account-service/user/src/main/java/com/user/infrastructure/kafka/configuration/KafkaConfiguration.java
@@ -1,40 +1,16 @@
 package com.user.infrastructure.kafka.configuration;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-/**
- * 실제 kafka는 운영 환경에서 동작합니다.
- * 로컬 실행 시 profile을 바꿀 수 있습니다.
- */
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class KafkaConfiguration {
-
-	@Value("${custom.cart.topic.command}")
-	private String cartCommandTopic;
-
-	@Value("${custom.config.topic-partitions}")
-	private int topicPartitions;
-	@Value("${custom.config.topic-replications}")
-	private int topicReplications;
-
 	@Bean
 	public KafkaTemplate<String, Object> kafkaTemplate(
 		ProducerFactory<String, Object> producerFactory
 	) {
 		return new KafkaTemplate<>(producerFactory);
-	}
-
-	@Bean
-	public NewTopic createCartsCommandTopic() {
-		return TopicBuilder.name(cartCommandTopic)
-			.partitions(topicPartitions)
-			.replicas(topicReplications)
-			.build();
 	}
 }
