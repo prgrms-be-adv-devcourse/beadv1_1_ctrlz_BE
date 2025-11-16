5d055f5cee6372229537acc8b1ec513d27f5d59c
junho <>
2025-11-16 17:06:41 +0900

    hotfix: merge 후 생기는 오류 해결

--- domain-service/src/main/java/com/domainservice/DomainServiceApplication.java
+++ domain-service/src/main/java/com/domainservice/DomainServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.domainservice", "com.common"})
 @EnableFeignClients(
 	basePackages = {"com.domainservice.common.feign.client"}
