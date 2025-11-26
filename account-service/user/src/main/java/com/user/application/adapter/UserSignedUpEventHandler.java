d3464ab037750271f0bcd924927f3726216ec240
최민석 <106965005+choizz156@users.noreply.github.com>
2025-11-26 12:07:37 +0900

    Feature/#130 kafka 비동기 (#131)
    * test: cd test
    
    * refactor: 유저 도메인에서 본인의 상태를 변경 #119
    
    * style: pagkage import #119
    
    * feat: kafka 이벤트 발송 및 이벤트 상태 변화 비동기 처리
    
    * refactor: kafka 멱등 처리 설정 및 수동 커밋, 리스너 네이밍 변경 #130

--- account-service/user/src/main/java/com/user/application/adapter/UserSignedUpEventHandler.java
+++ account-service/user/src/main/java/com/user/application/adapter/UserSignedUpEventHandler.java
@@ -1,9 +1,8 @@
 package com.user.application.adapter;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -32,7 +31,8 @@ public class UserSignedUpEventHandler {
 		externalEventPersistentPort.save(event.userId(), event.eventType());
 	}
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
+
+	@Async("taskExecutor")
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void publishKafka(UserSignedUpEvent event) {
 		CartCreateCommand cartCreateCommand = new CartCreateCommand(event.userId());
