d795819229eb3a2b23600b8a492eac6e1ac6b542
junho <88640680+iamian815@users.noreply.github.com>
2025-11-28 16:28:55 +0900

    refactor: Reivew 관련 기능 고도화 (#135) (#145)
    - gateway-service로부터 사용자 정보를 받아오는 코드 추기
    - Review 작성 전 해당 사용자가 정말 물건을 구매확정했는지 검증 로직 추가
    - Review 업데이트 전 해당 사용자가 작성자인지 검증로직 추가
    - Review 고도화에 따른 예외 클래스 추가
    
    Co-authored-by: junho <>

--- domain-service/src/main/java/com/domainservice/domain/order/repository/OrderRepository.java
+++ domain-service/src/main/java/com/domainservice/domain/order/repository/OrderRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.domainservice.domain.order.model.entity.Order;
 
-public interface OrderRepository extends JpaRepository<Order, String> {
+public interface OrderRepository extends JpaRepository<Order, String>, OrderQueryRepository {
     Optional<Order> findById(String orderId);
 }
