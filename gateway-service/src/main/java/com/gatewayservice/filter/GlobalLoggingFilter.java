4a3751e6476786fd3f5dc4dfb0301c6663ff165e
choizz156 <tkfka156@gmail.com>
2025-11-30 05:02:20 +0900

    refactor: 로깅 설정 수정 #153

---
+++ gateway-service/src/main/java/com/gatewayservice/filter/GlobalLoggingFilter.java
@@ -0,0 +1,36 @@
+package com.gatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalLoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        long startTime = System.currentTimeMillis();
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+
+            log.info("[Gateway] {} {} {} {}ms",
+                    exchange.getRequest().getMethod(),
+                    exchange.getRequest().getURI(),
+                    exchange.getResponse().getStatusCode(),
+                    duration);
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
