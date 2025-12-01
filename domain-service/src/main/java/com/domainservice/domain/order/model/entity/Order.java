1b3bdc2c16a5ad46f4980a4f4c640d3d473d1d34
sanga <tkddk9141@naver.com>
2025-12-01 19:36:15 +0900

    feat: order 기능추가(#141)

--- domain-service/src/main/java/com/domainservice/domain/order/model/entity/Order.java
+++ domain-service/src/main/java/com/domainservice/domain/order/model/entity/Order.java
@@ -7,16 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.common.model.persistence.BaseEntity;
-import com.domainservice.domain.payment.model.entity.PaymentEntity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -32,59 +29,56 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Order extends BaseEntity {
 
-	@Column(name = "user_id", nullable = false)
-	private String buyerId;  // 구매자id
+    @Column(name = "user_id", nullable = false)
+    private String buyerId;  // 구매자id
 
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-	@Builder.Default
-	private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-	@Column(nullable = false)
-	private String orderName;
+    @Column(nullable = false)
+    private String orderName;
 
-	@Column(nullable = false, length = 30)
-	@Enumerated(EnumType.STRING)
-	private OrderStatus orderStatus;
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private PaymentEntity payment;
+    @Column(name = "payment_id")
+    private String paymentId;
 
-	public void addOrderItem(OrderItem item) {
-		this.orderItems.add(item);
-		item.setOrder(this);
-	}
+    public void addOrderItem(OrderItem item) {
+        this.orderItems.add(item);
+        item.setOrder(this);
+    }
 
-	public BigDecimal getTotalAmount() {
-		return orderItems.stream()
-			.map(OrderItem::getTotalPrice)
-			.reduce(BigDecimal.ZERO, BigDecimal::add);
-	}
+    public BigDecimal getTotalAmount() {
+        return orderItems.stream()
+            .map(OrderItem::getTotalPrice)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 
-	public void setOrderStatus(OrderStatus orderStatus) {
-		this.orderStatus = orderStatus;
-	}
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 
-	public void orderConfirmed() {
-		orderStatus = PURCHASE_CONFIRMED;
-		orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.PURCHASE_CONFIRMED));
-	}
+    public void orderConfirmed() {
+        orderStatus = PURCHASE_CONFIRMED;
+        orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.PURCHASE_CONFIRMED));
+    }
 
-	public void orderCanceled() {
-		orderStatus = CANCELLED;
-		orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.CANCELLED));
-	}
+    public void orderCanceled() {
+        orderStatus = CANCELLED;
+        orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.CANCELLED));
+    }
 
-	public void orderRefundedAfterPayment() {
-		orderStatus = REFUND_AFTER_PAYMENT;
-		orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.REFUND_AFTER_PAYMENT));
-	}
+    public void orderRefundedAfterPayment() {
+        orderStatus = REFUND_AFTER_PAYMENT;
+        orderItems.forEach(item -> item.setOrderItemStatus(OrderItemStatus.REFUND_AFTER_PAYMENT));
+    }
 
-	@Override
-	protected String getEntitySuffix() {
-		return "order";
-	}
+    @Override
+    protected String getEntitySuffix() {
+        return "order";
+    }
 
-	public void setPayment(PaymentEntity paymentEntity) {
-		this.payment = paymentEntity;
-	}
 }
