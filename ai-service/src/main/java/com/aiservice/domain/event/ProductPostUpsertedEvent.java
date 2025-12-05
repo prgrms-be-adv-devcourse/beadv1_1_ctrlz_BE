70e660c9686401241b947833c963739a85c6eb94
choizz156 <tkfka156@gmail.com>
2025-12-05 20:54:57 +0900

    feat: vector db 저장/수정/삭제 로직 구성 #159

---
+++ ai-service/src/main/java/com/aiservice/domain/event/ProductPostUpsertedEvent.java
@@ -0,0 +1,28 @@
+package com.aiservice.domain.event;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Builder;
+
+@Builder
+public record ProductPostUpsertedEvent(
+		String id,
+		String name,
+		String title,
+		String description,
+		List<String> tags,
+		String categoryName,
+		Long price,
+		Long likedCount,
+		Long viewCount,
+		String status,
+		String tradeStatus,
+		String deleteStatus,
+
+		@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS") LocalDateTime createdAt,
+
+		EventType eventType) {
+}
--- common/src/main/java/com/common/event/productPost/ProductPostUpsertedEvent.java
+++ common/src/main/java/com/common/event/productPost/ProductPostUpsertedEvent.java
@@ -25,6 +25,6 @@ public record ProductPostUpsertedEvent(
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
 	LocalDateTime createdAt,
 
-	EventType eventType
+	String eventType
 ) {
 }
