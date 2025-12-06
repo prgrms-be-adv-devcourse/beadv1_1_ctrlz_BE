52a9ecab89afb04a60113b4f90f83259eaaedba7
choizz156 <tkfka156@gmail.com>
2025-12-07 01:23:52 +0900

    fix: batch-service를 실행 가능한 루트 모듈로 변경 및 빌드 오류 수정 #168

--- batch-service/src/main/java/com/batch/BatchApplication.java
+++ batch-service/src/main/java/com/batch/BatchApplication.java
@@ -1,11 +1,13 @@
-package com.settlementservice;
+package com.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = { "com.batch", "com.settlementservice", "com.common", "com.search" })
 public class BatchApplication {
 
 	public static void main(String[] args) {
