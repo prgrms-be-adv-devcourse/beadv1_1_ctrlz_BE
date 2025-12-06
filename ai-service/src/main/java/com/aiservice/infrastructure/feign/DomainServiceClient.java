82d96f86517a8c9c3dd1a3a7f485662654d1913b
choizz156 <tkfka156@gmail.com>
2025-12-06 12:31:36 +0900

    feat: fegin client 설정 #159

---
+++ ai-service/src/main/java/com/aiservice/infrastructure/feign/DomainServiceClient.java
@@ -0,0 +1,20 @@
+package com.aiservice.infrastructure.feign;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aiservice.infrastructure.feign.dto.ProductPostEsSearchResponse;
+import com.aiservice.infrastructure.feign.dto.PageResponse;
+
+
+@FeignClient(name = "domain-service", url = "${custom.feign.url.domain-service}")
+public interface DomainServiceClient {
+
+    @GetMapping("/api/product-posts/search")
+    PageResponse<List<ProductPostEsSearchResponse>> search(
+            @RequestParam(value = "q", required = false) String query,
+            @RequestParam(value = "size", defaultValue = "20") int size);
+}
