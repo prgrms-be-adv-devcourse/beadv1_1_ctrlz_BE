c22a1cbcc125d8f54fee892eb6276b4822f0a7f3
choizz156 <tkfka156@gmail.com>
2025-12-06 12:31:36 +0900

    feat: fegin client 설정 #159

---
+++ ai-service/src/main/java/com/aiservice/infrastructure/feign/configuration/FeignConfiguration.java
@@ -0,0 +1,9 @@
+package com.aiservice.infrastructure.feign.configuration;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@EnableFeignClients
+@Configuration
+public class FeignConfiguration {
+}
