b1afd39fd8c86ec3e196f05a15fa5a6066a879b8
devoks <ok6737@naver.com>
2025-12-09 17:23:56 +0900

    Feature/#160 상품 좋아요 및 찜한 상품 목록 기능 (#171)
    * remove: deposit, payment 테스트 코드 삭제  (#160)
    
    * refactor: docker-compose.yml에 mysql 설정 추가하고 initData 동작하도록 수정 (#160)
    
    * feat: 상품 좋아요 성공, 취소, 관심상품 조회 api 기능 구현 (#160)
    
    * test: 상품 좋아요 테스트 코드 작성 (#160)
    
    * refactor: common 모듈의 EventType를 참조하도록 수정 (#160)
    
    * refactor: dev (윈도우) 환경에서 임베디드 레디스가 아닌 실제 redis를 사용하도록 Profile 수정 (#160)
    
    * feat: gateway routes 설정에 favorite api 추가 (#160)
    
    * feat: 좋아요 재시도 시 방어 로직 추가(#160)
    
    * chore: kafka 통신에 사용되는 event 객체 경로를 * -> ** 로 와일드카드 수정(#160)
    
    * feat: 동시에 여러사용자가 좋아요 요청을 했을 때 count 정합성을 맞추기 위해 @Modifying 을 사용하여 해결 (#160)
    
    * feat: 동일 사용자가 동시에 여러번 좋아요 요청을 했을 때 1번만 성공할 수 있도록 로직 수정 (#160)
    
    - favoriteRepository에 값을 save하면서 flush를 통해 쿼리를 발생하도록 구현
    - 중복 요청이 들어오면 db에 userId,PostId에 대한 유니크 인덱스에 제약이 걸려 예외가 발생함
    
    * test: 좋아요 로직에 대한 정합성 테스트 구현 (#160)
    
    * test: feign 클라이언트 decoder 생성으로 발생하는 테스트 오류 해결 (#160)
    
    * feat : DataIntegrityViolationException 를 GlobalExceptionHandler에서 관리하도록 분리 (#160)
    
    * test: 변경된 로직에 맞게 테스트 수정 (#160)
    
    * test: feign으로 유저 가져오는 로직이 변경됨에 따라 테스트 코드 변경 (#160)
    
    * comment: FavoriteProduct 로직에 주석 추가 (#160)
    
    * comment: TODO 제거 (#160)

--- domain-service/src/test/java/com/domainservice/domain/post/post/service/GetProductPostTest.java
+++ domain-service/src/test/java/com/domainservice/domain/post/post/service/GetProductPostTest.java
@@ -15,16 +15,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.common.exception.CustomException;
 import com.common.model.vo.ProductStatus;
 import com.common.model.vo.TradeStatus;
 import com.domainservice.common.configuration.feign.client.UserFeignClient;
+import com.domainservice.common.configuration.feign.exception.UserClientException;
 import com.domainservice.domain.post.post.exception.ProductPostException;
 import com.domainservice.domain.post.post.model.dto.response.ProductPostResponse;
 import com.domainservice.domain.post.post.model.entity.ProductPost;
 import com.domainservice.domain.post.post.repository.ProductPostRepository;
-
-import feign.FeignException;
 
 /**
  * ProductPostService 조회 기능 테스트
@@ -174,7 +172,7 @@ class GetProductPostTest {
         verify(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
     }
 
-    @DisplayName("존재하지 않는 사용자가 조회하면 CustomException이 발생한다.")
+    @DisplayName("존재하지 않는 사용자가 조회하면 예외가 발생한다.")
     @Test
     void test4() throws Exception {
         // given
@@ -194,18 +192,20 @@ class GetProductPostTest {
         setViewCount(productPost, 15);
 
         given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willThrow(FeignException.NotFound.class);
 
         // when & then
-        assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
-                .isInstanceOf(CustomException.class);
+		given(userClient.getUser(userId))
+			.willThrow(new UserClientException.NotFound("Not Found"));
+
+		assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
+			.isInstanceOf(UserClientException.NotFound.class);
 
         verify(productPostRepository).findById(postId);
         verify(userClient).getUser(userId);
         verify(recentlyViewedService, never()).addRecentlyViewedPost(anyString(), anyString(), anyInt());
     }
 
-    @DisplayName("User Service 인증 실패 시 ProductPostException이 발생한다.")
+    @DisplayName("User Service 인증 실패 시 예외가 발생한다.")
     @Test
     void test5() throws Exception {
         // given
@@ -225,103 +225,20 @@ class GetProductPostTest {
         setViewCount(productPost, 25);
 
         given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willThrow(FeignException.Unauthorized.class);
+		given(userClient.getUser(userId)).willThrow(new UserClientException.Unauthorized("Unauthorized"));
 
         // when & then
         assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
-                .isInstanceOf(ProductPostException.class)
-                .hasMessage(EXTERNAL_API_ERROR.getMessage());
+                .isInstanceOf(UserClientException.Unauthorized.class);
 
         verify(productPostRepository).findById(postId);
         verify(userClient).getUser(userId);
         verify(recentlyViewedService, never()).addRecentlyViewedPost(anyString(), anyString(), anyInt());
-    }
-
-    @DisplayName("User Service 권한 없음 시 ProductPostException이 발생한다.")
-    @Test
-    void test6() throws Exception {
-        // given
-        String userId = "user-999";
-        String postId = "post-123";
-
-        ProductPost productPost = ProductPost.builder()
-                .userId("user-123")
-                .categoryId("category-123")
-                .title("아이패드 프로")
-                .name("iPad Pro")
-                .price(1200000)
-                .status(ProductStatus.GOOD)
-                .tradeStatus(TradeStatus.SELLING)
-                .build();
-
-        setViewCount(productPost, 30);
-
-        given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willThrow(FeignException.Forbidden.class);
-
-        // when & then
-        assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
-                .isInstanceOf(ProductPostException.class)
-                .hasMessage(EXTERNAL_API_ERROR.getMessage());
-
-        verify(productPostRepository).findById(postId);
-        verify(userClient).getUser(userId);
-        verify(recentlyViewedService, never()).addRecentlyViewedPost(anyString(), anyString(), anyInt());
-    }
-
-    @DisplayName("User Service 통신 오류 시 ProductPostException이 발생한다.")
-    @Test
-    void test7() throws Exception {
-        // given
-        String userId = "user-111";
-        String postId = "post-123";
-
-        ProductPost productPost = ProductPost.builder()
-                .userId("user-123")
-                .categoryId("category-123")
-                .title("애플워치")
-                .name("Apple Watch")
-                .price(500000)
-                .status(ProductStatus.GOOD)
-                .tradeStatus(TradeStatus.SELLING)
-                .build();
-
-        setViewCount(productPost, 40);
-
-        given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willThrow(mock(FeignException.class));
-
-        // when & then
-        assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
-                .isInstanceOf(ProductPostException.class)
-                .hasMessage(EXTERNAL_API_ERROR.getMessage());
-
-        verify(productPostRepository).findById(postId);
-        verify(userClient).getUser(userId);
-        verify(recentlyViewedService, never()).addRecentlyViewedPost(anyString(), anyString(), anyInt());
-    }
-
-    @DisplayName("존재하지 않는 게시글은 조회할 수 없다.")
-    @Test
-    void test8() {
-        // given
-        String userId = "user-123";
-        String postId = "invalid-post-id";
-
-        given(productPostRepository.findById(postId)).willReturn(Optional.empty());
-
-        // when & then
-        assertThatThrownBy(() -> productPostService.getProductPostById(userId, postId))
-                .isInstanceOf(ProductPostException.class)
-                .hasMessage(PRODUCT_POST_NOT_FOUND.getMessage());
-
-        verify(productPostRepository).findById(postId);
-        verify(userClient, never()).getUser(anyString());
     }
 
     @DisplayName("삭제된 게시글은 조회할 수 없다.")
     @Test
-    void test9() {
+    void test6() {
         // given
         String userId = "user-123";
         String postId = "post-123";
@@ -351,7 +268,7 @@ class GetProductPostTest {
 
     @DisplayName("판매 완료된 게시글도 조회할 수 있다.")
     @Test
-    void test10() throws Exception {
+    void test7() throws Exception {
         // given
         String userId = "anonymous";
         String postId = "post-123";
@@ -383,7 +300,7 @@ class GetProductPostTest {
 
     @DisplayName("거래 진행 중인 게시글도 조회할 수 있다.")
     @Test
-    void test11() throws Exception {
+    void test8() throws Exception {
         // given
         String userId = "anonymous";
         String postId = "post-123";
@@ -413,45 +330,9 @@ class GetProductPostTest {
         verify(userClient, never()).getUser(anyString());
     }
 
-    @DisplayName("인증된 사용자가 판매 완료 게시글을 조회하면 최근 본 상품에 저장된다.")
-    @Test
-    void test12() throws Exception {
-        // given
-        String userId = "user-789";
-        String postId = "post-123";
-
-        ProductPost productPost = ProductPost.builder()
-                .userId("user-123")
-                .categoryId("category-123")
-                .title("아이폰 15 Pro (판매완료)")
-                .name("iPhone 15 Pro")
-                .price(1200000)
-                .status(ProductStatus.GOOD)
-                .tradeStatus(TradeStatus.SOLDOUT)
-                .build();
-
-        setId(productPost, postId);
-        setViewCount(productPost, 100);
-
-        given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willReturn(UserViewFactory.createUser(userId));
-        doNothing().when(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
-
-        // when
-        ProductPostResponse result = productPostService.getProductPostById(userId, postId);
-
-        // then
-        assertThat(result).isNotNull();
-        assertThat(result.tradeStatus()).isEqualTo(TradeStatus.SOLDOUT);
-
-        verify(productPostRepository).findById(postId);
-        verify(userClient).getUser(userId);
-        verify(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
-    }
-
     @DisplayName("SELLER 권한 사용자도 게시글을 조회할 수 있다.")
     @Test
-    void test13() throws Exception {
+    void test9() throws Exception {
         // given
         String userId = "seller-123";
         String postId = "post-123";
@@ -485,39 +366,4 @@ class GetProductPostTest {
         verify(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
     }
 
-    @DisplayName("ADMIN 권한 사용자도 게시글을 조회할 수 있다.")
-    @Test
-    void test14() throws Exception {
-        // given
-        String userId = "admin-123";
-        String postId = "post-123";
-
-        ProductPost productPost = ProductPost.builder()
-                .userId("user-789")
-                .categoryId("category-123")
-                .title("닌텐도 스위치")
-                .name("Nintendo Switch")
-                .price(350000)
-                .status(ProductStatus.GOOD)
-                .tradeStatus(TradeStatus.SELLING)
-                .build();
-
-        setId(productPost, postId);
-        setViewCount(productPost, 50);
-
-        given(productPostRepository.findById(postId)).willReturn(Optional.of(productPost));
-        given(userClient.getUser(userId)).willReturn(UserViewFactory.createAdmin(userId));
-        doNothing().when(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
-
-        // when
-        ProductPostResponse result = productPostService.getProductPostById(userId, postId);
-
-        // then
-        assertThat(result).isNotNull();
-        assertThat(result.title()).isEqualTo("닌텐도 스위치");
-
-        verify(productPostRepository).findById(postId);
-        verify(userClient).getUser(userId);
-        verify(recentlyViewedService).addRecentlyViewedPost(userId, postId, MAX_COUNT);
-    }
 }
