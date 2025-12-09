66af7c78d6c62e4b1644f0da09dbd4d7ce0789e8
junho <>
2025-12-09 15:42:55 +0900

    refactor: BaseEntity의 id 생성 관련 로직 수정 (#167) - suffix 제거 - uuid 형태로 id 생성하는 코드 메서드화

--- common/src/main/java/com/common/model/persistence/BaseEntity.java
+++ common/src/main/java/com/common/model/persistence/BaseEntity.java
@@ -37,12 +37,13 @@ public abstract class BaseEntity {
 
 	@PrePersist // persist() 를 호출하기 전, 새로운 엔티티가 영속성 컨텍스트에 관리되기 직전에 호출
 	protected void onCreate() {
-
-		String UUIDv7 = UuidCreator.getTimeOrderedEpoch().toString();
-		this.id = UUIDv7 + "-" + getEntitySuffix();
-
+		this.id = createEntityId();
 		this.createdAt = LocalDateTime.now();
 		this.updatedAt = LocalDateTime.now();
+	}
+
+	public static String createEntityId() {
+		return UuidCreator.getTimeOrderedEpoch().toString();
 	}
 
 	public void delete() {
