b4c61953cee0160fcbe52f768766d6dbb7f291d9
junho <88640680+iamian815@users.noreply.github.com>
2025-12-10 08:39:08 +0900

    Feature/#167 db와 es 간 정합성 맞추는 로직 구현 (#173)
    * setting: ai-service에 kafka 이벤트 전송 로직 추가 (#167)
    - 회원 PK, 회원이 검색한 키워드
    
    * refactor: BaseEntity의 id 생성 관련 로직 수정 (#167)
    - suffix 제거
    - uuid 형태로 id 생성하는 코드 메서드화
    
    * feat: kafka consumer, producer 관련 어노테이션 추가 (#167)
    - @component와 동일
    
    * refactor: ProductPostEventConsumer 패키지 변경 (#167)
    
    * refactor: 검색어 자동완성 기능 관련 코드 리팩토링(#167)
    - DB <-> ES간 데이터 정합성 맞추는 로직 구현
    - 실시간/일간 인기 검색어 목록 업데이트 로직 Lua Script로 변경.
    - 비회원의 검색창 초기화면에 실시간 트렌드 검색어 목록 보여지도록 수정
    - Service Layer에서 Redis와 통신하는 로직과 Elasticsearch에 통신하는 로직을 별도의 서비스로 분리
    - 테스트용 검색어 더미데이터 저장 api 구현
    - SearchWordDocumentEntity 불필요한 필드변수 제거
    
    * refactor: suffix 생성하는 메서드 @Deprecated 적용 (#167)
    
    * fix: kafka 이벤트 매개변수 위치 수정 (#167)
    
    ---------
    
    Co-authored-by: junho <>

--- domain-service/src/main/java/com/domainservice/domain/search/repository/impl/SearchWordQueryRepositoryImpl.java
+++ domain-service/src/main/java/com/domainservice/domain/search/repository/impl/SearchWordQueryRepositoryImpl.java
@@ -6,6 +6,8 @@ import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.stereotype.Repository;
 
 import com.domainservice.domain.search.model.entity.dto.document.SearchWordDocumentEntity;
@@ -47,18 +49,18 @@ public class SearchWordQueryRepositoryImpl implements SearchWordQueryRepository 
 	}
 
 	@Override
-	public List<SearchWordDocumentEntity> findAllByKoreanWord(String koreanWord) {
+	public List<SearchWordDocumentEntity> findAllByOriginValue(String koreanWord) {
 
 		Query fuzzyPrefixQuery = BoolQuery.of(b -> b
 			.should(
 				PrefixQuery.of(p -> p
-					.field("koreanWord")
+					.field("originValue")
 					.value(koreanWord)
 				)._toQuery()
 			)
 			.should(
 				FuzzyQuery.of(f -> f
-					.field("koreanWord")
+					.field("originValue")
 					.value(koreanWord)
 					.fuzziness("AUTO")
 					.prefixLength(Math.min(koreanWord.length(), 2))
@@ -69,6 +71,29 @@ public class SearchWordQueryRepositoryImpl implements SearchWordQueryRepository 
 		return executeSearch(fuzzyPrefixQuery);
 	}
 
+	/**
+	 * @description originValue 기준으로 upsert 처리
+	 * Document에 originValue가 있으면 update 없으면 insert
+	 * @param searchWordDocumentEntities
+	 */
+	@Override
+	public void upsertByOriginValue(List<SearchWordDocumentEntity> searchWordDocumentEntities) {
+
+		List<IndexQuery> indexQueries = searchWordDocumentEntities.stream()
+			.map(entity ->
+				IndexQuery.builder()
+					.withId(entity.getOriginValue())   // originValue를 문서 ID로 사용
+					.withObject(entity)                // 전체 document 저장
+					.build()
+			)
+			.toList();
+
+		// indexName = "search-words"
+		operations.bulkIndex(
+			indexQueries,
+			IndexCoordinates.of("search-words")
+		);
+	}
 
 	private List<SearchWordDocumentEntity> executeSearch(Query query) {
 		NativeQuery searchQuery = NativeQuery.builder()
