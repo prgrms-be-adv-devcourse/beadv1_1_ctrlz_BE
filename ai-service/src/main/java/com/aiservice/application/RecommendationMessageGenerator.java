6fd431e704e36f448f98ce50c5639f7c9ac701b7
choizz156 <tkfka156@gmail.com>
2025-12-09 16:23:05 +0900

    feat: 로컬 운영 환경 임베딩 모델 llm 분리 #159

--- ai-service/src/main/java/com/aiservice/application/RecommendationMessageGenerator.java
+++ ai-service/src/main/java/com/aiservice/application/RecommendationMessageGenerator.java
@@ -7,7 +7,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.aiservice.controller.dto.DocumentSearchResponse;
@@ -29,6 +28,7 @@ public class RecommendationMessageGenerator {
 
 	private final ChatClient chatClient;
 	private final UserContextService userContextService;
+	private final PromptTemplate promptTemplate;
 	private final ChatOptions chatOptions;
 
 	public String toPrompt(String userId, String query, List<DocumentSearchResponse> recommendations) {
@@ -40,10 +40,6 @@ public class RecommendationMessageGenerator {
 		String userContext = buildUserContext(userId);
 		String searchContext = "사용자가 '%s'라고 검색했습니다.".formatted(query);
 		String productList = buildProductList(recommendations);
-
-		PromptTemplate promptTemplate = new PromptTemplate(
-			new ClassPathResource("prompts/recommendation.st")
-		);
 
 		String finalPrompt = promptTemplate.render(Map.of(
 			"searchContext", searchContext,
@@ -64,17 +60,17 @@ public class RecommendationMessageGenerator {
 	}
 
 	private String buildUserContext(String userId) {
-		UserContext ctx = userContextService.getUserContext(userId);
+		UserContext context = userContextService.getUserContext(userId);
 		
 		return String.format(
 			"""
 			사용자 정보: [성별: %s, 나이: %d세, 최근 검색어: %s, 최근 30일 간 장바구니 상품들: %s]
 			최근 조회한 상품들 : [%s]
 			""",
-			ctx.gender(), ctx.age(),
-			String.join(", ", ctx.searchKeywords()),
-			String.join(", ", ctx.cartProductNames()),
-			String.join(", ", ctx.viewedTitle())
+			 context.gender(),  context.age(),
+			String.join(", ",  context.searchKeywords()),
+			String.join(", ",  context.cartProductNames()),
+			String.join(", ",  context.viewedTitle())
 		);
 	}
 
