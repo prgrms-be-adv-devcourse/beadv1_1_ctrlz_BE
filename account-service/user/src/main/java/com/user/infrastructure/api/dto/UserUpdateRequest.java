7dfe03f384194f13a41b413ab11fe49ddafaa7e3
Choiminseok <tkfka156@gmail.com>
2025-12-12 23:22:28 +0900

    refactor: account-service 운영 환경 설정 수정 및 코드 수정 #211
    # Conflicts:
    #	.github/workflows/payment-cd-workflow.yml
    #	docker/ai-service.dockerfile
    #	docker/batch-service.dockerfile
    #	docker/discovery-service.dockerfile
    #	docker/gateway-service.dockerfile

--- account-service/user/src/main/java/com/user/infrastructure/api/dto/UserUpdateRequest.java
+++ account-service/user/src/main/java/com/user/infrastructure/api/dto/UserUpdateRequest.java
@@ -7,15 +7,10 @@ public record UserUpdateRequest(
 	String nickname,
 	@NotBlank(message = "연락처를 입력해주세요.")
 	String phoneNumber,
-	@NotBlank(message = "거리를 입력해주세요.")
 	String street,
-	@NotBlank(message = "우편번호를 입력해주세요.")
 	String zipCode,
-	@NotBlank(message = "군/구/면을 입력해주세요.")
 	String state,
-	@NotBlank(message = "광역시를 입력해 주세요")
 	String city,
-	@NotBlank(message = "상세주소를 입력해주세요.")
 	String details
 ) {
 }
