696e5b2970f669d95af0bd725f72caf83483fa00
jeonghyeon-ohk <ok6737@naver.com>
2025-12-13 22:52:39 +0900

    feat: gateway를 통해서 domain-service의 swagger ui를 확인할 수 있도록 설정 (#196)

--- domain-service/src/main/java/com/domainservice/common/configuration/springDoc/SpringDocConfiguration.java
+++ domain-service/src/main/java/com/domainservice/common/configuration/springDoc/SpringDocConfiguration.java
@@ -2,8 +2,9 @@ package com.domainservice.common.configuration.springDoc;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,30 +12,30 @@ import org.springframework.util.StreamUtils;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SpringDocConfiguration {
 
 	@Bean
-	public GroupedOpenApi groupController() {
-		return GroupedOpenApi.builder()
-			.group("controller")
-			.pathsToMatch("/api/**")
-			.build();
-	}
-
-	@Bean
-	public OpenAPI customOpenAPI() throws IOException {
+	public OpenAPI customOpenAPI(
+		@Value("${openapi.service.url}") String gatewayUrl
+	) throws IOException {
 
 		ClassPathResource resource = new ClassPathResource("docs/responseDescription.txt");
 		String description = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
 
 		return new OpenAPI()
+			.servers(List.of(
+				new Server().url(gatewayUrl).description("Gateway Server"),
+				new Server().url("http://localhost:8081").description("Direct Access")
+			))
+			.servers(List.of(new Server().url(gatewayUrl).description("Gateway Server")))
 			.info(new Info()
-				.title("연근마켓 이커머스 플랫폼 API 서버")
+				.title("연근마켓 - Domain Service API")
 				.version("v1.0.0")
 				.description(description)
 			);
-
 	}
+
 }
