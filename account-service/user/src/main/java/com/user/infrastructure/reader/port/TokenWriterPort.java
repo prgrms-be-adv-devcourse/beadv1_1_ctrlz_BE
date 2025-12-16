ffe7c529dd958ff49275d096478e654daf10e7f1
최민석 <106965005+choizz156@users.noreply.github.com>
2025-12-17 08:51:32 +0900

    Refactor/#211 k8s v1 (#215)
    * refactor: account-service 운영 환경 설정 수정 및 코드 수정 #211
    
    # Conflicts:
    #	.github/workflows/payment-cd-workflow.yml
    #	docker/ai-service.dockerfile
    #	docker/batch-service.dockerfile
    #	docker/discovery-service.dockerfile
    #	docker/gateway-service.dockerfile
    
    * refactor: user 관련 로직 수정 및 es 더미데이터 수정 #211
    
    # Conflicts:
    #	.github/workflows/payment-cd-workflow.yml
    #	docker/ai-service.dockerfile
    #	docker/batch-service.dockerfile
    #	docker/discovery-service.dockerfile
    #	docker/gateway-service.dockerfile
    
    * release: domain 배포 #211
    
    * refactor: swagger local 설정 #211
    
    * refactor: 더미 es 추가 #211
    
    * fix: 유저 정보 조회 오류 수정 #211
    
    * fix: 쿠키 네이밍 변경 #211
    
    * fix: item view 로그 추가 및 필터 변경 #211
    
    * refactor: pod probe 변경 #211
    
    * test: api 테스트 #211

--- account-service/user/src/main/java/com/user/infrastructure/reader/port/TokenWriterPort.java
+++ account-service/user/src/main/java/com/user/infrastructure/reader/port/TokenWriterPort.java
@@ -3,5 +3,6 @@ package com.user.infrastructure.reader.port;
 import com.user.infrastructure.reader.port.dto.TokenResponse;
 
 public interface TokenWriterPort {
-	TokenResponse issueToken(String userId);
+	TokenResponse issueUserRoleToken(String userId);
+	TokenResponse issueSellerRoleToken(String userId);
 }
