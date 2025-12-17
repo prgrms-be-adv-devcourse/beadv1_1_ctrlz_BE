de0b0acf6e9df663883b7459aab46d4ddfb6bfe5
choizz156 <tkfka156@gmail.com>
2025-12-17 19:34:07 +0900

    test: 테스트 코드 수정 #218

--- account-service/account-application/src/test/java/com/accountapplication/auth/AuthControllerTest.java
+++ account-service/account-application/src/test/java/com/accountapplication/auth/AuthControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -63,19 +62,15 @@ class AuthControllerTest {
 	@Test
 	void test2() throws Exception {
 		// given
-		String token = "valid-access-token";
 		String userId = "testUser";
-
-		given(jwtTokenProvider.getUserIdFromToken(token)).willReturn(userId);
 
 		// when then
 		mockMvc.perform(get("/api/auth/logout")
-				.header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+				.header("X-REQUEST-ID", userId))
 			.andDo(print())
 			.andExpect(status().isOk())
 			.andExpect(header().exists("Set-Cookie"));
 
 		verify(jwtAuthService).logout(userId);
-		verify(jwtTokenProvider).getUserIdFromToken(token);
 	}
 }
